a4e227aa5e04e61f9f9e8ed16bf5c2b01445c866
hoanhua14 <huasohoan@gmail.com>
2023-09-26 17:53:13 -0700

    set up default formatter

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
 const Pet = (props) => {
-    return React.createElement('div', {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h3", {}, props.breed),
-
-    ]);
-}
-
-const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                animal: "Dog",
-                name: "Pia",
-                breed: "Beijing"
-            }),
-            React.createElement(Pet, {
-                animal: "Dog",
-                name: "Lu",
-                breed: "Japan"
-            }),
-            React.createElement(Pet, {
-                animal: "Dog",
-                name: "Ky",
-                breed: "Mixed"
-            }),
-            React.createElement(Pet, {
-                animal: "Cat",
-                name: "Kid",
-                breed: "Scottish Fold"
-            }),
-
-        ])
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
 
-const container = document.getElementById('root');
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Pia",
+      breed: "Beijing",
+    }),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Lu",
+      breed: "Japan",
+    }),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Ky",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Kid",
+      breed: "Scottish Fold",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
