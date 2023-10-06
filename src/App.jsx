57cc0428de24a3c7341b58c294b8884c8c890d5c
hoanhua14 <huasohoan@gmail.com>
2023-10-05 18:34:50 -0700

    react Query

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
+      <QueryClientProvider client={queryClient}>
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
@@ -14,7 +24,7 @@ const App = () => {
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
         </Routes>
-      </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
