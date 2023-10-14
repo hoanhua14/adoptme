cf9e5332461c6c378624d257dd84ab17ff611d0c
hoanhua14 <huasohoan@gmail.com>
2023-10-13 21:14:21 -0700

    update

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -5,3 +5,4 @@ export default function useBreedList(animal) {
   const results = useQuery(["breeds", animal], fetchBreedList);
   return [results?.data?.breeds ?? [], results.status];
 }
+// refactor
